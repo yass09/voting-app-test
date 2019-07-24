03fe7d212188a33e335e2634f99a8b562147a190
Yacine Chibane-Devereux <y.chibane@me.com>
2019-07-24 12:59:50 +0200

    added presentationals to Not Found page

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -4,13 +4,13 @@ import { NavigationLink } from '../components/presentational/NavigationLink';
 import * as routes from '../constants/routes';
 import Main from '../components/presentational/Main';
 
-import surveyLogo from '../assets/vote.svg';
+import surveyImg from '../assets/vote.svg';
 import { MainImg } from '../components/presentational/MainImg'
 import { Title } from '../components/presentational/Title';
 
 const Home = () => (
   <Main flexDirection={'column'}>
-    <MainImg src={surveyLogo}/>
+    <MainImg src={surveyImg}/>
     <Title>Welcome to this Anime survey</Title>
     <NavigationLink className='btn btn-primary btn-lg' to={routes.SURVEY}>Take Survey !</NavigationLink>
   </Main>
--- src/Views/NotFound.js
+++ src/Views/NotFound.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
+import Main from '../components/presentational/Main';
+import { MainImg } from '../components/presentational/MainImg'
+import { Title } from '../components/presentational/Title';
+
+import notFoundImg from '../assets/404.svg';
+
+
 const NotFound = () => (
-  <div>
-    NotFound
-  </div>
+  <Main flexDirection={'column'}>
+    <MainImg src={notFoundImg}/>
+    <Title>Oops...Page not Found</Title>
+  </Main>
 );
 
 export default NotFound;
